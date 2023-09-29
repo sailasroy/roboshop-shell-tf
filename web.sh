2f15b9602fec62a7f9c3229758fcf7b051a6afda
sailasroy <sailasroy123@gmail.com>
2023-09-29 13:04:59 +0530

    Adding roboshop copy for terraform

--- web.sh
+++ web.sh
@@ -47,7 +47,7 @@ cd /usr/share/nginx/html &>>$LOGFILE
 unzip /tmp/web.zip &>>$LOGFILE
     VALIDATE $? "Unzipping the html file"
 
-cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$LOGFILE
+cp /home/centos/roboshop-shell-tf/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$LOGFILE
     VALIDATE $? "Copying roboshop.conf"
 
 systemctl restart nginx &>>$LOGFILE
