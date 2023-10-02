5b96db592e0d6b4095094b919a1d1404f13edbac
sailasroy <sailasroy123@gmail.com>
2023-10-02 12:05:09 +0530

    adding

--- cart.sh
+++ cart.sh
@@ -1,75 +1,77 @@
 #!/bin/bash
+
 DATE=$(date +%F)
+LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
-LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
-# https://github.com/sailasroy/roboshop-shell.git=/tmp/$SCRIPT_NAME-$DATE.log
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
-USERNAME=$(id)
-
-USERID=$(id -u)
-if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ];
 then
-echo -e "$R ERROR:: Please sign in with root access $N"
-exit 1
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
 fi
 
 VALIDATE(){
-if [ $1 -ne 0 ]
-then
-echo -e "$2 ........$R FAILURE $N"
-exit 1
-else 
-echo -e "$2............$G SUCCESS $N"
-fi
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
-    VALIDATE $? "Dowloading nodejs source file"
-yum install nodejs -y &>>$LOGFILE
-    VALIDATE $? "Installing nodejs"  
-useradd roboshop
-if [ id -ne 0] 
-else
-echo ""
-fi
 
-mkdir /app
+VALIDATE $? "Setting up NPM Source"
+
+yum install nodejs -y &>>$LOGFILE
+
+VALIDATE $? "Installing NodeJS"
+
+#once the user is created, if you run this script 2nd time
+# this command will defnitely fail
+# IMPROVEMENT: first check the user already exist or not, if not exist then create
+useradd roboshop &>>$LOGFILE
+
+#write a condition to check directory already exist or not
+mkdir /app &>>$LOGFILE
 
 curl -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip &>>$LOGFILE
-    VALIDATE $? "Downloading the cart artifact"
+
+VALIDATE $? "downloading cart artifact"
 
 cd /app &>>$LOGFILE
-    VALIDATE $? "Opening app directory"
+
+VALIDATE $? "Moving into app directory"
 
 unzip /tmp/cart.zip &>>$LOGFILE
-    VALIDATE $? "Unzipping cart artifact"
 
-cd /app &>>$LOGFILE
-    VALIDATE $? "Opening app directory"
+VALIDATE $? "unzipping cart"
 
 npm install &>>$LOGFILE
-    VALIDATE $? "Installing nodejs dependencies"
 
+VALIDATE $? "Installing dependencies"
+
+# give full path of cart.service because we are inside /app
 cp /home/centos/roboshop-shell-tf/cart.service /etc/systemd/system/cart.service &>>$LOGFILE
-    VALIDATE $? "Copying cart.service"
+
+VALIDATE $? "copying cart.service"
 
 systemctl daemon-reload &>>$LOGFILE
-    VALIDATE $? "Reloading cart.service"
+
+VALIDATE $? "daemon reload"
 
 systemctl enable cart &>>$LOGFILE
-    VALIDATE $? "Enabling cart.service"
+
+VALIDATE $? "Enabling cart"
 
 systemctl start cart &>>$LOGFILE
-    VALIDATE $? "Starting cart.service"
 
-cp /home/centos/roboshop-shell-tf/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
-    VALIDATE $? "Copying mongo.repo"
-
-yum install mongodb-org-shell -y &>>$LOGFILE
-    VALIDATE $? "Installing mongodb client"
-
-# mongo --host mongodb.sailasdevops.online </app/schema/cart.js &>>$LOGFILE
-#     VALIDATE $? "Uploading cart products through mongodb"
+VALIDATE $? "Starting cart"
