0e4f82f83805ea1b52b3a7744b89555127c89df7
sailasroy <sailasroy123@gmail.com>
2023-10-02 14:44:46 +0530

    adding

--- mysql.sh
+++ mysql.sh
@@ -1,52 +1,46 @@
 #!/bin/bash
 
 DATE=$(date +%F)
-LOGSDIR=/tmp
-# /home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
-LOGFILE=$LOGSDIR/$0-$DATE.log
-USERID=$(id -u)
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
-Y="\e[33m"
 
-if [ $USERID -ne 0 ];
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
 then
-    echo -e "$R ERROR:: Please run this script with root access $N"
-    exit 1
+echo -e "$R ERROR:: Please sign in with root access $N"
+exit 1
 fi
 
 VALIDATE(){
-    if [ $1 -ne 0 ];
-    then
-        echo -e "$2 ... $R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N"
-    fi
+if [ $1 -ne 0 ]
+then
+echo -e "$2 ........$R FAILURE $N"
+exit 1
+else 
+echo -e "$2............$G SUCCESS $N"
+fi
 }
 
-yum module disable mysql -y &>> $LOGFILE
+yum module disable mysql -y &>>$LOGFILE
+VALIDATE $? "Disabling mysql"
 
-VALIDATE $? "Disabling the default version"
+cp /home/centos/roboshop-shell-tf/mysql.repo /etc/yum.repos.d/mysql.repo  &>>$LOGFILE
+VALIDATE $? "Copying mysql.repo"
 
-cp /home/centos/roboshop-shell-tf/mysql.repo /etc/yum.repos.d/mysql.repo &>> $LOGFILE
+yum install mysql-community-server -y &>>$LOGFILE
+VALIDATE $? "Installing mysql"
 
-VALIDATE $? "Copying MySQL repo" 
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabling mysql"
 
-yum install mysql-community-server -y &>> $LOGFILE
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "Starting mysql"
 
-VALIDATE $? "Installing MySQL Server"
+mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOGFILE
+VALIDATE $? "Setting cartname and password"
 
-systemctl enable mysqld &>> $LOGFILE
-
-VALIDATE $? "Enabling MySQL"
-
-systemctl start mysqld &>> $LOGFILE
-
-VALIDATE $? "Staring MySQL"
-
-mysql_secure_installation --set-root-pass RoboShop@1 &>> $LOGFILE
-
-VALIDATE $? "setting up root password"
+# mysql -uroot -pRoboShop@1 &>>$LOGFILE
+# VALIDATE $? "Checking cartname and password"
